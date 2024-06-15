b0fcf96f904fd81451d736da6c92e80002f580b1
Thomas Keig <keigthomas@gmail.com>
2024-06-15 01:30:58 +0100

    Update Cosmo version API for v2.0.0

--- api/cosmo/greeting.py
+++ api/cosmo/greeting.py
@@ -3,16 +3,16 @@ print("                                                                         
 print("――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――")
 print("                                                                          ")
 print(BOLD + BLUE + UNDERLINE + f"Cosmo v{version} is now Online!" + RESET)
-print(WHITE + "• Join our discord server for support: " + GREEN + "https://discord.gg/a5htZzcgum")
-print(WHITE + "• By using this bot you agree to all terms located here: " + GREEN + "https://atmtx.co/cosmo-terms")
-print(WHITE + "• Addons for the bot can be found here: " + GREEN + "https://atmtx.co/cosmo-addons")
+print(WHITE + "• Join our discord server for support: " + GREEN + "https://discord.gg/a5htZzcgum" + RESET)
+print(WHITE + "• By using this bot you agree to all terms located here: " + GREEN + "https://atmtx.co/cosmo-terms" + RESET)
+print(WHITE + "• Addons for the bot can be found here in the Discord Server" + RESET)
 print("                                                                          ")
 print(BOLD + BLUE + UNDERLINE + "API Addon:" + RESET)
 
 if api_load:
-    print(WHITE + "• Successfully Loaded Cosmo API")
+    print(WHITE + "• Successfully Loaded Cosmo API" + RESET)
 else:
-    print(WHITE + "• Purchase the Cosmo API Addon on BuiltByBit: " + GREEN + "https://atmtx.co/cosmo")
+    print(WHITE + "• Purchase the Cosmo API Addon on BuiltByBit: " + GREEN + "https://atmtx.co/cosmo" + RESET)
 
 print("                                                                          ")
 # print(BOLD + BLUE + UNDERLINE + "Important Information:" + RESET)
